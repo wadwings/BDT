aa6ea14c9447289f6e20cef026f2e0a13460a432
Skydevourer-0 <r195572306@qq.com>
2020-05-16 16:31:20 +0800

    发现爬取的文件名中多了个空格……将错就错修改了索引文件中的路径

--- get_index.py
+++ get_index.py
@@ -18,7 +18,7 @@ for i in range(34):
         city=city.string.replace(' ','')
         infor[province][city]={
             'name':city,
-            'path':'/mapdata/{}.json'.format(city)
+            'path':'/mapdata/{} .json'.format(city)
         }
 
 json_data=json.dumps(infor,indent=4,ensure_ascii=False)
